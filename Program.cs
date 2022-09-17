633437aee2b7fb7e9eaff41a24f1f8665865c393
Cix8 <francesco.natal_fn@libero.it>
2022-09-17 16:01:06 +0200

    add Factorial; add myNumb dynamic

--- Program.cs
+++ Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿Console.WriteLine("Inserisci un numero intero");
+int myNumb = Convert.ToInt32(Console.ReadLine());
+int result = myNumb;
+
+void Factorial(int number)
+{
+    if (number > 1)
+    {
+        result = result * (number - 1);
+        number = number - 1;
+        Factorial(number);
+    }
+}
+
+
+Factorial(myNumb);
+
+Console.WriteLine($"Il fattoriale di {myNumb} è uguale a : {result}");
